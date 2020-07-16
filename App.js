3d4dc34a01ba6c0be5b54614b960a54865ac8a26
vanshbafna <59309266+vanshbafna@users.noreply.github.com>
2020-07-16 15:10:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Grocery from './screens/Grocery';
+import Expences from './screens/Expences';
+import Welcome from './screens/Welcome';
+import Fitness from './screens/Fitness';
+
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+return (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Welcome" component={Welcome} />
+      <Tab.Screen name="Grocery" component={ Grocery} />
+      <Tab.Screen name="Expences" component={ Expences} />
+      <Tab.Screen name="Fitness" component={ Fitness} />
+   
+    
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
